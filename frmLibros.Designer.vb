a900ff80452e26955eaa5c3dee7c9904e5fb5e40
claualta <claualta@gmail.com>
2026-09-11 10:19:54 -0300

    Clase 04 - Join 2 tablas - carga en formulario

--- frmLibros.Designer.vb
+++ frmLibros.Designer.vb
@@ -27,39 +27,58 @@ Partial Class frmLibros
         btnBuscar = New Button()
         btnRefrescar = New Button()
         dgvLibros = New DataGridView()
+        txtIdLibro = New TextBox()
+        txtTitulo = New TextBox()
+        txtIdioma = New TextBox()
+        txtFormato = New TextBox()
+        nudEdicion = New NumericUpDown()
+        cboEditorial = New ComboBox()
+        btnGuardar = New Button()
+        btnModificar = New Button()
+        btnEliminar = New Button()
+        Label2 = New Label()
+        Label3 = New Label()
+        Label4 = New Label()
+        Label5 = New Label()
+        Label6 = New Label()
+        Label7 = New Label()
         CType(dgvLibros, ComponentModel.ISupportInitialize).BeginInit()
+        CType(nudEdicion, ComponentModel.ISupportInitialize).BeginInit()
         SuspendLayout()
         ' 
         ' txtFiltro
         ' 
-        txtFiltro.Location = New Point(116, 30)
+        txtFiltro.Location = New Point(102, 22)
+        txtFiltro.Margin = New Padding(3, 2, 3, 2)
         txtFiltro.Name = "txtFiltro"
-        txtFiltro.Size = New Size(225, 27)
+        txtFiltro.Size = New Size(197, 23)
         txtFiltro.TabIndex = 0
         ' 
         ' Label1
         ' 
         Label1.AutoSize = True
-        Label1.Location = New Point(50, 33)
+        Label1.Location = New Point(44, 25)
         Label1.Name = "Label1"
-        Label1.Size = New Size(43, 20)
+        Label1.Size = New Size(34, 15)
         Label1.TabIndex = 1
         Label1.Text = "Libro"
         ' 
         ' btnBuscar
         ' 
-        btnBuscar.Location = New Point(362, 30)
+        btnBuscar.Location = New Point(317, 22)
+        btnBuscar.Margin = New Padding(3, 2, 3, 2)
         btnBuscar.Name = "btnBuscar"
-        btnBuscar.Size = New Size(106, 29)
+        btnBuscar.Size = New Size(93, 22)
         btnBuscar.TabIndex = 2
         btnBuscar.Text = "BUSCAR"
         btnBuscar.UseVisualStyleBackColor = True
         ' 
         ' btnRefrescar
         ' 
-        btnRefrescar.Location = New Point(487, 28)
+        btnRefrescar.Location = New Point(426, 21)
+        btnRefrescar.Margin = New Padding(3, 2, 3, 2)
         btnRefrescar.Name = "btnRefrescar"
-        btnRefrescar.Size = New Size(106, 29)
+        btnRefrescar.Size = New Size(93, 22)
         btnRefrescar.TabIndex = 3
         btnRefrescar.Text = "REFRESCAR"
         btnRefrescar.UseVisualStyleBackColor = True
@@ -67,29 +86,171 @@ Partial Class frmLibros
         ' dgvLibros
         ' 
         dgvLibros.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize
-        dgvLibros.Location = New Point(16, 83)
+        dgvLibros.Location = New Point(14, 62)
+        dgvLibros.Margin = New Padding(3, 2, 3, 2)
         dgvLibros.MultiSelect = False
         dgvLibros.Name = "dgvLibros"
         dgvLibros.ReadOnly = True
         dgvLibros.RowHeadersWidth = 51
         dgvLibros.SelectionMode = DataGridViewSelectionMode.FullRowSelect
-        dgvLibros.Size = New Size(841, 454)
+        dgvLibros.Size = New Size(866, 251)
         dgvLibros.TabIndex = 4
+        ' 
+        ' txtIdLibro
+        ' 
+        txtIdLibro.Location = New Point(108, 332)
+        txtIdLibro.Name = "txtIdLibro"
+        txtIdLibro.ReadOnly = True
+        txtIdLibro.Size = New Size(52, 23)
+        txtIdLibro.TabIndex = 5
+        ' 
+        ' txtTitulo
+        ' 
+        txtTitulo.Location = New Point(108, 361)
+        txtTitulo.Name = "txtTitulo"
+        txtTitulo.Size = New Size(426, 23)
+        txtTitulo.TabIndex = 6
+        ' 
+        ' txtIdioma
+        ' 
+        txtIdioma.Location = New Point(108, 390)
+        txtIdioma.Name = "txtIdioma"
+        txtIdioma.Size = New Size(141, 23)
+        txtIdioma.TabIndex = 7
+        ' 
+        ' txtFormato
+        ' 
+        txtFormato.Location = New Point(108, 419)
+        txtFormato.Name = "txtFormato"
+        txtFormato.Size = New Size(141, 23)
+        txtFormato.TabIndex = 8
+        ' 
+        ' nudEdicion
+        ' 
+        nudEdicion.Location = New Point(108, 448)
+        nudEdicion.Name = "nudEdicion"
+        nudEdicion.Size = New Size(65, 23)
+        nudEdicion.TabIndex = 9
+        ' 
+        ' cboEditorial
+        ' 
+        cboEditorial.DropDownStyle = ComboBoxStyle.DropDownList
+        cboEditorial.FormattingEnabled = True
+        cboEditorial.Location = New Point(109, 479)
+        cboEditorial.Name = "cboEditorial"
+        cboEditorial.Size = New Size(425, 23)
+        cboEditorial.TabIndex = 10
+        ' 
+        ' btnGuardar
+        ' 
+        btnGuardar.Location = New Point(573, 326)
+        btnGuardar.Name = "btnGuardar"
+        btnGuardar.Size = New Size(103, 33)
+        btnGuardar.TabIndex = 11
+        btnGuardar.Text = "GUARDAR"
+        btnGuardar.UseVisualStyleBackColor = True
+        ' 
+        ' btnModificar
+        ' 
+        btnModificar.Location = New Point(573, 365)
+        btnModificar.Name = "btnModificar"
+        btnModificar.Size = New Size(103, 33)
+        btnModificar.TabIndex = 12
+        btnModificar.Text = "MODIFICAR"
+        btnModificar.UseVisualStyleBackColor = True
+        ' 
+        ' btnEliminar
+        ' 
+        btnEliminar.Location = New Point(573, 404)
+        btnEliminar.Name = "btnEliminar"
+        btnEliminar.Size = New Size(103, 33)
+        btnEliminar.TabIndex = 13
+        btnEliminar.Text = "ELIMINAR"
+        btnEliminar.UseVisualStyleBackColor = True
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Location = New Point(54, 335)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(48, 15)
+        Label2.TabIndex = 14
+        Label2.Text = "ID Libro"
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Location = New Point(65, 365)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(37, 15)
+        Label3.TabIndex = 15
+        Label3.Text = "Titulo"
+        ' 
+        ' Label4
+        ' 
+        Label4.AutoSize = True
+        Label4.Location = New Point(58, 393)
+        Label4.Name = "Label4"
+        Label4.Size = New Size(44, 15)
+        Label4.TabIndex = 16
+        Label4.Text = "Idioma"
+        ' 
+        ' Label5
+        ' 
+        Label5.AutoSize = True
+        Label5.Location = New Point(50, 422)
+        Label5.Name = "Label5"
+        Label5.Size = New Size(52, 15)
+        Label5.TabIndex = 17
+        Label5.Text = "Formato"
+        ' 
+        ' Label6
+        ' 
+        Label6.AutoSize = True
+        Label6.Location = New Point(56, 450)
+        Label6.Name = "Label6"
+        Label6.Size = New Size(46, 15)
+        Label6.TabIndex = 18
+        Label6.Text = "Edicion"
+        ' 
+        ' Label7
+        ' 
+        Label7.AutoSize = True
+        Label7.Location = New Point(53, 482)
+        Label7.Name = "Label7"
+        Label7.Size = New Size(50, 15)
+        Label7.TabIndex = 19
+        Label7.Text = "Editorial"
         ' 
         ' frmLibros
         ' 
-        AutoScaleDimensions = New SizeF(8F, 20F)
+        AutoScaleDimensions = New SizeF(7F, 15F)
         AutoScaleMode = AutoScaleMode.Font
-        ClientSize = New Size(914, 600)
+        ClientSize = New Size(905, 520)
+        Controls.Add(Label7)
+        Controls.Add(Label6)
+        Controls.Add(Label5)
+        Controls.Add(Label4)
+        Controls.Add(Label3)
+        Controls.Add(Label2)
+        Controls.Add(btnEliminar)
+        Controls.Add(btnModificar)
+        Controls.Add(btnGuardar)
+        Controls.Add(cboEditorial)
+        Controls.Add(nudEdicion)
+        Controls.Add(txtFormato)
+        Controls.Add(txtIdioma)
+        Controls.Add(txtTitulo)
+        Controls.Add(txtIdLibro)
         Controls.Add(dgvLibros)
         Controls.Add(btnRefrescar)
         Controls.Add(btnBuscar)
         Controls.Add(Label1)
         Controls.Add(txtFiltro)
-        Margin = New Padding(3, 4, 3, 4)
         Name = "frmLibros"
         Text = "Libros"
         CType(dgvLibros, ComponentModel.ISupportInitialize).EndInit()
+        CType(nudEdicion, ComponentModel.ISupportInitialize).EndInit()
         ResumeLayout(False)
         PerformLayout()
     End Sub
@@ -99,4 +260,19 @@ Partial Class frmLibros
     Friend WithEvents btnBuscar As Button
     Friend WithEvents btnRefrescar As Button
     Friend WithEvents dgvLibros As DataGridView
+    Friend WithEvents txtIdLibro As TextBox
+    Friend WithEvents txtTitulo As TextBox
+    Friend WithEvents txtIdioma As TextBox
+    Friend WithEvents txtFormato As TextBox
+    Friend WithEvents nudEdicion As NumericUpDown
+    Friend WithEvents cboEditorial As ComboBox
+    Friend WithEvents btnGuardar As Button
+    Friend WithEvents btnModificar As Button
+    Friend WithEvents btnEliminar As Button
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label7 As Label
 End Class
--- frmLibros.vb
+++ frmLibros.vb
@@ -13,12 +13,16 @@ Public Class frmLibros
                 'armo mi consulta sql
                 Dim consulta As String
 
+                consulta = "SELECT li.clavelibro, li.titulo, li.idioma, li.formato, li.edicion, li.claveeditorial, ed.nombre AS Editorial " &
+                            "FROM libro AS li " &
+                            "JOIN editorial AS ed ON li.claveeditorial = ed.claveeditorial "
+
                 'aplico filtro
                 If filtro = "" Then
-                    consulta = "SELECT * FROM libro " &
+                    consulta = consulta &
                             " ORDER BY titulo;"
                 Else
-                    consulta = "SELECT * FROM libro " &
+                    consulta = consulta &
                                 "WHERE titulo Like @filtro " &
                                 "ORDER BY titulo;"
                 End If
@@ -37,10 +41,12 @@ Public Class frmLibros
                     'cargar la tabla en la grilla
                     dgvLibros.DataSource = tabla
 
+                    'oculto la columna clave editorial
+                    If (dgvLibros.Columns.Contains("claveeditorial")) Then
+                        dgvLibros.Columns("claveeditorial").Visible = False
+                    End If
                 End Using
-
             End Using
-
         Catch ex As Exception
             'muestro mensaje de error
             MessageBox.Show("ERROR: " & ex.Message)
@@ -48,19 +54,139 @@ Public Class frmLibros
 
     End Sub
 
+    Private Sub CargarComboEditoriales()
+        Try
+            Using cn As New MySqlConnection(CADENA)
+                cn.Open()
+                Dim sql As String =
+                        "SELECT claveeditorial, nombre FROM editorial ORDER BY nombre"
+                Using cmd As New MySqlCommand(sql, cn)
+                    Dim tabla As New DataTable()
+                    Using lector = cmd.ExecuteReader()
+                        tabla.Load(lector)
+                    End Using
+                    ' El usuario verá el nombre...
+                    cboEditorial.DisplayMember = "nombre"
+                    ' ...pero el programa guardará la clave numérica.
+                    cboEditorial.ValueMember = "claveeditorial"
+                    cboEditorial.DataSource = tabla
+                End Using
+            End Using
+        Catch ex As Exception
+            MessageBox.Show("Error al cargar editoriales: " & ex.Message)
+        End Try
+    End Sub
+
+    Sub LimpiarForm()
+        txtFiltro.Clear()
+        txtIdLibro.Clear()
+        txtTitulo.Clear()
+        txtIdioma.Clear()
+        txtFormato.Clear()
+        nudEdicion.Value = 0
+        txtFiltro.Focus()
+    End Sub
+
     Private Sub frmLibros_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         'cargo la grilla sin filtrar
         CargarGrilla()
+        'cargo combo editorial
+        CargarComboEditoriales()
     End Sub
 
     Private Sub btnBuscar_Click(sender As Object, e As EventArgs) Handles btnBuscar.Click
         'llamo a cargargrilla con filtro
         CargarGrilla(txtFiltro.Text.Trim)
-
     End Sub
 
     Private Sub txtFiltro_TextChanged(sender As Object, e As EventArgs) Handles txtFiltro.TextChanged
         'llamo a cargargrilla con filtro
         CargarGrilla(txtFiltro.Text.Trim)
+    End Sub
+
+    Private Sub btnGuardar_Click(sender As Object, e As EventArgs) Handles btnGuardar.Click
+        If txtTitulo.Text.Trim() = "" Then
+            MessageBox.Show("Ingrese el título.")
+            txtTitulo.Focus()
+            Exit Sub
+        End If
+
+        If cboEditorial.SelectedValue Is Nothing Then
+            MessageBox.Show("Seleccione una editorial.")
+            Exit Sub
+        End If
+
+        Try
+            Using cn As New MySqlConnection(CADENA)
+                cn.Open()
+                Dim sql As String =
+                "INSERT INTO libro(titulo, idioma, formato, edicion, claveeditorial) " &
+                "VALUES(@titulo, @idioma, @formato, @edicion, @editorial)"
+                Using cmd As New MySqlCommand(sql, cn)
+                    cmd.Parameters.AddWithValue("@titulo", txtTitulo.Text.Trim())
+                    cmd.Parameters.AddWithValue("@idioma", txtIdioma.Text.Trim())
+                    cmd.Parameters.AddWithValue("@formato", txtFormato.Text.Trim())
+                    cmd.Parameters.AddWithValue("@edicion", CInt(nudEdicion.Value))
+                    ' SelectedValue contiene la claveeditorial del elemento elegido.
+                    cmd.Parameters.AddWithValue("@editorial", CInt(cboEditorial.SelectedValue))
+                    'capturo respuesta del servidor
+                    Dim resultado As Integer = cmd.ExecuteNonQuery()
+                    MessageBox.Show("Registros guardados: " & resultado.ToString())
+                End Using
+            End Using
+            MessageBox.Show("Libro guardado.")
+            CargarGrilla()
+            LimpiarForm()
+        Catch ex As Exception
+            MessageBox.Show("Error al guardar libro: " & ex.Message)
+        End Try
+    End Sub
+
+    Private Sub dgvLibros_CellClick(sender As Object, e As DataGridViewCellEventArgs) Handles dgvLibros.CellClick
+        'valido que haya seleccionado algo
+        If e.RowIndex < 0 Then Exit Sub
+        'extraigo los datos del libro de la grilla y los cargo en los textbox
+        Dim fila = dgvLibros.Rows(e.RowIndex)
+        txtIdLibro.Text = fila.Cells("clavelibro").Value.ToString()
+        txtTitulo.Text = fila.Cells("titulo").Value.ToString()
+        txtIdioma.Text = fila.Cells("idioma").Value.ToString()
+        txtFormato.Text = fila.Cells("formato").Value.ToString()
+        nudEdicion.Value = fila.Cells("edicion").Value
+        ' Para editar correctamente conviene que la consulta del DataGridView
+        ' también incluya claveeditorial, aunque la columna se oculte.
+        cboEditorial.SelectedValue = CInt(fila.Cells("claveeditorial").Value)
+    End Sub
+
+    Private Sub btnModificar_Click(sender As Object, e As EventArgs) Handles btnModificar.Click
+        If txtIdLibro.Text = "" Then
+            MessageBox.Show("Seleccione un libro.")
+            Exit Sub
+        End If
+
+        Try
+            Using cn As New MySqlConnection(CADENA)
+                cn.Open()
+                Dim sql As String =
+                "UPDATE libro SET titulo=@titulo, idioma=@idioma, formato=@formato, " &
+                "edicion=@edicion, claveeditorial=@editorial " &
+                "WHERE clavelibro=@id"
+                Using cmd As New MySqlCommand(sql, cn)
+                    cmd.Parameters.AddWithValue("@titulo", txtTitulo.Text.Trim())
+                    cmd.Parameters.AddWithValue("@idioma", txtIdioma.Text.Trim())
+                    cmd.Parameters.AddWithValue("@formato", txtFormato.Text.Trim())
+                    cmd.Parameters.AddWithValue("@edicion", CInt(nudEdicion.Value))
+                    'guardo la clave editorial
+                    cmd.Parameters.AddWithValue("@editorial", CInt(cboEditorial.SelectedValue))
+                    cmd.Parameters.AddWithValue("@id", CInt(txtIdLibro.Text))
+                    'capturo respuesta del servidor
+                    Dim resultado As Integer = cmd.ExecuteNonQuery()
+                    MessageBox.Show("Registros modificados: " & resultado.ToString())
+                End Using
+            End Using
+            CargarGrilla()
+            LimpiarForm()
+        Catch ex As Exception
+            MessageBox.Show("Error al modificar libro: " & ex.Message)
+        End Try
     End Sub
 End Class
